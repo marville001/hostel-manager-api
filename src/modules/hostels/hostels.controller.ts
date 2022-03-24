8dac5daece7002bd810de6e259825f0600e9640b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-24 18:06:04 +0300

    Hostel controllers

--- src/modules/hostels/hostels.controller.ts
+++ src/modules/hostels/hostels.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Hostel as HostelEntity } from './hostel.entity';
+import { HostelsService } from './hostels.service';
+import { HostelDto } from './dto/hostel.dto';
 
 @Controller('hostels')
-export class HostelsController {}
+export class HostelsController {
+  constructor(private readonly hostelsService: HostelsService) {}
+
+  @Get()
+  async findAll() {
+    return await this.hostelsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<HostelEntity> {
+    const post = await this.hostelsService.findOne(id);
+
+    if (!post) {
+      throw new NotFoundException('Hostel Not found');
+    }
+
+    return post;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  async create(
+    @Body() hostel: HostelDto,
+    @Request() req,
+  ): Promise<HostelEntity> {
+    // create a new post and return the newly created post
+    return await this.hostelsService.create(hostel);
+  }
+@UseGuards(AuthGuard('jwt'))
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() hostel: HostelDto, @Request() req): Promise<HostelEntity> {
+        // get the number of row affected and the updated post
+        const { numberOfAffectedRows, updatedPost } = await this.hostelsService.update(id, hostel, hostel.ownerId);
+
+        // if the number of row affected is zero, 
+        // it means the post doesn't exist in our db
+        if (numberOfAffectedRows === 0) {
+            throw new NotFoundException('This Hostel doesn\'t exist');
+        }
+
+        // return the updated post
+        return updatedPost;
+    }	
+
+}
--- src/modules/hostels/hostels.module.ts
+++ src/modules/hostels/hostels.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { HostelsController } from './hostels.controller';
 import { hostelsProviders } from './hostels.providers';
 import { HostelsService } from './hostels.service';
 
 @Module({
+  imports:[UsersModule],
   controllers: [HostelsController],
-  providers: [...hostelsProviders, HostelsService],
+  providers: [HostelsService, ...hostelsProviders],
 })
 export class HostelsModule {}
