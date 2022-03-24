ab1e2b049e0d0d323e29088363d895becc61dcda
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-24 18:41:09 +0300

    Updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,5 +2,8 @@ import { Controller, Get, Param, Query, Redirect } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
-  
+	@Get()
+	appRoot(): string{
+		return "App running..."
+	}
 }
