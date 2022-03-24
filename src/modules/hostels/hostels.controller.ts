bb7fd0f25c2d79064621f9a51a37cb093747918b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-24 18:12:12 +0300

    Delete hostels done

--- src/modules/hostels/hostels.controller.ts
+++ src/modules/hostels/hostels.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
@@ -43,20 +44,41 @@ export class HostelsController {
     // create a new post and return the newly created post
     return await this.hostelsService.create(hostel);
   }
-@UseGuards(AuthGuard('jwt'))
-    @Put(':id')
-    async update(@Param('id') id: number, @Body() hostel: HostelDto, @Request() req): Promise<HostelEntity> {
-        // get the number of row affected and the updated post
-        const { numberOfAffectedRows, updatedPost } = await this.hostelsService.update(id, hostel, hostel.ownerId);
 
-        // if the number of row affected is zero, 
-        // it means the post doesn't exist in our db
-        if (numberOfAffectedRows === 0) {
-            throw new NotFoundException('This Hostel doesn\'t exist');
-        }
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() hostel: HostelDto,
+    @Request() req,
+  ): Promise<HostelEntity> {
+    // get the number of row affected and the updated post
+    const { numberOfAffectedRows, updatedPost } =
+      await this.hostelsService.update(id, hostel, hostel.ownerId);
 
-        // return the updated post
-        return updatedPost;
-    }	
+    // if the number of row affected is zero,
+    // it means the post doesn't exist in our db
+    if (numberOfAffectedRows === 0) {
+      throw new NotFoundException("This Hostel doesn't exist");
+    }
 
+    // return the updated post
+    return updatedPost;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  async remove(@Param('id') id: number, @Request() req) {
+    // delete the post with this id
+    const deleted = await this.hostelsService.delete(id, req.user.id);
+
+    // if the number of row affected is zero,
+    // then the post doesn't exist in our db
+    if (deleted === 0) {
+      throw new NotFoundException("This Hostel doesn't exist");
+    }
+
+    // return success message
+    return 'Successfully deleted';
+  }
 }
